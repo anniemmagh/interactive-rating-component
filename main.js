e2024ddb97d904fc301f492fbb3b5b12027b7dab
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2023-05-25 15:02:43 +0400

    add js code

---
+++ main.js
@@ -0,0 +1,18 @@
+const mainContainer = document.querySelector(".container")
+const thanksContainer = document.querySelector(".thank-you")
+const submitButton = document.getElementById("submit-rating")
+const rateAgain = document.getElementById("rate-again")
+const ratings = document.querySelectorAll(".btn")
+const actualRating = document.getElementById("rating")
+
+submitButton.addEventListener("click", () => {
+  mainContainer.style.display = "none"
+  thanksContainer.classList.remove("hidden")
+
+  ratings.forEach((rating) => {
+    rating.addEventListener("click", () => {
+      actualRating.innerHTML = rating.innerHTML
+    })
+  })
+})
+
